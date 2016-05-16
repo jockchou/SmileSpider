ac93af18cb4a6e062fa6b1d72a8b8822f081a8ed
zhoujing223 <zhoujing223@pingan.com.cn>
2016-05-16 21:54:51 +0800

    m

--- tests/TestMessageQueueScheduler.php
+++ tests/TestMessageQueueScheduler.php
@@ -18,6 +18,8 @@ $msgQueue->push("0");
 function handleMessage($msgQueue, $message)
 {
     $pid = posix_getpid();
+
+    sleep(1);
     echo "handle message: " . $message . ", current p: $pid\n";
 
     if ($message === "0") {
@@ -31,8 +33,6 @@ $fork = new \Common\Fork;
 
 for ($i = 0; $i < 5; $i++) {
     $fork->call(function () use ($msgQueue) {
-        $pid = posix_getpid();
-        echo "current p: $pid\n";
         while ($msgQueue->count() > 0) {
             handleMessage($msgQueue, $msgQueue->poll());
         }
