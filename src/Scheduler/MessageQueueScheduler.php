a97625336010d2c43c2141f2b9a84ae1889805b9
zhoujing223 <zhoujing223@pingan.com.cn>
2016-05-16 11:18:43 +0800

    update

---
+++ src/Scheduler/MessageQueueScheduler.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jockchou
+ * Date: 2016/5/16
+ * Time: 10:05
+ */
+
+namespace Scheduler;
+
+use GuzzleHttp\Psr7\Request;
+
+class MessageQueueScheduler implements Scheduler
+{
+    private $queue;
+    private $key;
+    const MSG_TYPE = 1;
+    const MAX_SIZE = 2048;
+
+    function __construct()
+    {
+        $this->key = ftok(__FILE__, 'G');
+        if ($this->key === -1) {
+            throw new \Exception("ftok failed");
+        }
+        $this->queue = msg_get_queue($this->key);
+    }
+
+    public function push(Request $request)
+    {
+        return msg_send($this->queue, self::MSG_TYPE, $request);
+    }
+
+    public function poll()
+    {
+        msg_receive($this->queue, 0, $msgType, self::MAX_SIZE, $message, false, MSG_IPC_NOWAIT);
+
+        return $message;
+    }
+
+    public function count()
+    {
+        $msgStat = msg_stat_queue($this->queue);
+
+        return $msgStat['msg_qnum'];
+    }
+}
---
+++ tests/TestMessageQueueScheduler.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: jockchou
+ * Date: 2016/5/16
+ * Time: 11:00
+ */
+
+require_once(__DIR__ . '/../../vendor/autoload.php');
+
+$msgQueue = new MessageQueueScheduler();
+
+$pids = array();
+
+for ($i = 0; $i < 5; $i++) {
+    //创建子进程
+    $pids[$i] = pcntl_fork();
+
+    if ($pids[$i]) {
+        echo "No.$i child process was created, the pid is $pids[$i]\r\n";
+    } elseif ($pids[$i] == 0) {
+        $pid = posix_getpid();
+
+        echo "process.$pid is writing now\r\n";
+
+        msg_send($message_queue, 1, "this is process.$pid's data\r\n");
+
+        $msgQueue->push(new \GuzzleHttp\Psr7\Request("GET", "http://$i.com"));
+
+        posix_kill($pid, SIGTERM);
+    }
+}
+
+do {
+    msg_receive($message_queue, 0, $message_type, 1024, $message, true, MSG_IPC_NOWAIT);
+    echo $message;
+
+    //需要判断队列是否为空，如果为空就退出
+    if ($msgQueue->count() <= 0) {
+        break;
+    }
+} while (true);
